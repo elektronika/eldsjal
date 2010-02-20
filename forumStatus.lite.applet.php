765584c48d605a32a552fbd639c5fd16f238675e
Johnny <johnny@nurrd.se>
2010-02-20 17:45:27 +0000

    Cleanup

--- forumStatus.lite.applet.php
+++ forumStatus.lite.applet.php
@@ -12,7 +12,7 @@ if( $application['systemforumcount'] > 0 ) {
 		$messageFormat = str_replace( "[br]", " ", $messages['message'] );
 		$message = substr( $messageFormat, 0, 150 )."...";
 		$message = rq($message);
-		print "<tr><td colspan=\"2\">&raquo;&nbsp;<a href=\"forum.php?mode=readTopic&category=".$messages['forumcategoryid']."&threadid=".$messages['topicid']."\" class=\"a2\">".$messages['topicname']."</td></tr><tr><td CLASS='plaintext'><B>&nbsp;</B></td><td CLASS='plaintext'>".$message."</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td></tr>";
+		print "<tr><td colspan=\"2\">&raquo;&nbsp;<a href=\"/forum/topic/".$messages['topicid']."\" class=\"a2\">".$messages['topicname']."</td></tr><tr><td CLASS='plaintext'><B>&nbsp;</B></td><td CLASS='plaintext'>".$message."</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td></tr>";
 	}
 	print "<tr><td COLSPAN='2'>&nbsp;</td></tr></table>";
 	$messages = null;
