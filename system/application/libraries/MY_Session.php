dcf43affea5c7c4c9f92dd0441d178961dc0313d
Johnny <johnny@nurrd.se>
2010-02-20 17:48:53 +0000

    lalala

--- system/application/libraries/MY_Session.php
+++ system/application/libraries/MY_Session.php
@@ -36,7 +36,7 @@ class MY_Session extends CI_Session {
     }
 
 	public function usertype() {
-		return $this->userdata('usertype', -1);
+		return $this->userdata('usertype', 0);
 	}
 
 	public function isLoggedIn() {
