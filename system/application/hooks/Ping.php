daaef06113a30b34db584a4cecd309461e0526a2
Johnny <johnny@nurrd.se>
2010-03-22 09:56:36 +0000

    Bytte ut lastseen (datetime) mot ping (timestamp)

--- system/application/hooks/Ping.php
+++ system/application/hooks/Ping.php
@@ -3,7 +3,7 @@ function ping() {
 	$CI =& get_instance();
 	
 	if( $CI->session->isLoggedIn() ) {
-		$CI->db->query("UPDATE users SET online = 1, lastseen = NOW() WHERE userid = ".$CI->session->userid());
-		$CI->db->query("UPDATE users SET online = 0 WHERE TIMESTAMPDIFF(MINUTE, lastseen, NOW()) > 10");
+		$CI->db->query("UPDATE users SET online = 1, ping = ".time()." WHERE userid = ".$CI->session->userid());
+		$CI->db->query("UPDATE users SET online = 0 WHERE ping < ".(time() - 60 * 5));
 	}
 }
