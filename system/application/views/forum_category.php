6499fe44b0aa2f4c37f6fb414ca79306e40cc61b
Johnny <johnny@nurrd.se>
2010-04-15 14:54:39 +0000

    Linka till nyaste post om unread

--- system/application/views/forum_category.php
+++ system/application/views/forum_category.php
@@ -11,9 +11,9 @@
 	<tbody>
 <?php foreach($topics as $topic): ?>
 		<tr class="<?php echo implode(' ', $topic->classes); ?><?php echo nth(2) ? ' odd' : ' even'; ?>">
-			<td class="topic-subject"><span class="topic-link"><a href="/forum/topic/<?php echo $topic->id; ?>"><?php echo $topic->title; ?></a> 
+			<td class="topic-subject"><span class="topic-link"><a href="/forum/<?php echo isset($topic->classes['new']) ? 'redirectupdated' : 'topic'; ?>/<?php echo $topic->id; ?>"><?php echo $topic->title; ?></a> 
 			<?php echo pagespan($topic->posts, "/forum/topic/$topic->id", $posts_per_page); ?> 
-			<?php if(isset($topic->classes['new'])): ?> <a href="/forum/redirectupdated/<?php echo $topic->id; ?>"; ?>&raquo;</a><?php endif; ?></span> <?php echo actions($topic->actions, TRUE); ?></td>
+			</span> <?php echo actions($topic->actions, TRUE); ?></td>
 			<td class="topic-lastrepy"><?php if($topic->replies == 0): ?>-<?php else: ?><?php echo fuzzytime($topic->updated,'',' sedan'); ?> av <?php echo userlink($topic->updater); ?><?php endif; ?></td>
 			<td class="topic-replies"><?php echo $topic->replies; ?></td>
 			<td class="topic-creator"><?php echo shortdate($topic->created); ?> av <?php echo userlink($topic->creator); ?></td>
