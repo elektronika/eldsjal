f402a28eb41611136a5bd99fdbc0a8c0f00e17ac
Johnny <johnny@nurrd.se>
2010-05-12 16:53:40 +0000

    show/hide av kalender-mekk i topicformen

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+// Keepalive-funktionen
 window.setInterval("jQuery.get('/json/keepalive/'+Math.random())", 5 * 60 * 1000);
+
+$(function() {
+	// Gör så att datepickern döljs/visas beroende på om den behövs eller inte. Först när sidan laddas...
+	if(!$('input[name=is_event]').is(':checked')) {
+		$('.datepicker').hide();
+	}
+	// ...och sedan när checkboxen ändras.
+	$('input[name=is_event]').change(function() {
+		$('.datepicker').slideToggle('quick');
+	});
+});
