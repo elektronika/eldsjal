33eccb4622dca87d4ec828dbfd3eb87a6abde10d
Johnny <johnny@nurrd.se>
2010-06-03 17:13:54 +0000

    Snyggade till widget-settings

--- system/application/controllers/main.php
+++ system/application/controllers/main.php
@@ -1,25 +1,18 @@
 <?php
 class Main extends MY_Controller {
 	function get_index() {
-		// if($this->session->isLoggedIn())
-		// 			$this->widgets->set('left', $this->settings->get_array('widgets_left_front'));
-		// 		else
-		// 			$this->widgets->set('left', $this->settings->get_array('widgets_left_front_guest'));			
-		
 		$this->widgets->set('right', $this->settings->get_array('widgets_right_front'));
 
 		$this->widgets->remove('content', 'defaultheader');
 		$this->widgets->remove('content', 'region_content');
-		// $this->settings->get_array('widgets_front')
-		if($this->session->isLoggedIn())
-			$this->widgets->add('highlight', 'quicklinks');
-
-		foreach(array('new_timeline') as $widget)
-			$this->widgets->add('content', $widget);
+		
+		$this->widgets->set('content', $this->settings->get_array('widgets_front'));
 		
 		if( ! $this->session->isLoggedIn())
 			$this->widgets->add('highlight', 'frontintro');
-		
+		else
+			$this->widgets->add('highlight', 'quicklinks');
+
 		// Den här sidan består bara av widgets, så den behöver inte någon egen view.
 		$this->view->template = 'layout';
 	}
