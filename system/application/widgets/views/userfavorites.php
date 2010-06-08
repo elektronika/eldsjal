d5b7bf9f5333eb50c96456ceafab2cf30ebccc70
Johnny <johnny@nurrd.se>
2010-06-08 21:25:14 +0000

    Mer magi bland favvosarna

--- system/application/widgets/views/userfavorites.php
+++ system/application/widgets/views/userfavorites.php
@@ -9,7 +9,15 @@
 <ul class="flat">
 	<?php if( ! empty($items)): ?>
 	<?php foreach($items as $item): ?>
-		<li><?php echo userlink($item); ?> <a class="delete" href="/user/<?php echo $item->userid; ?>/unfav">x</a></li>
+		<li><?php echo userlink($item); ?> 
+			<span>
+				<a title="Gästbok" href="/guestbook/view/<?php echo $item->userid; ?>">GB</a>
+				<a title="Meddelande" href="/messages/new/<?php echo $item->userid; ?>">PM</a>
+				<a title="Bilder" href="/gallery/user:<?php echo $item->userid; ?>">B</a>
+				<a title="Tankar" href="/thoughts/user/<?php echo $item->userid; ?>">T</a>
+				<a class="delete" title="Sluta favorita" href="/user/<?php echo $item->userid; ?>/unfav">X</a>
+			</span>
+		</li>
 	<?php endforeach; ?>
 <?php else: ?>
 		<li>Tyvärr, ingen online just nu.</li>
