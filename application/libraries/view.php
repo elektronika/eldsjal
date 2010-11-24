253e46b6312b6f2b34ea8ded461d52da4ca7ef12
Johnny Karhinen <johnny@johnny-karhinens-macbook-2.local>
2010-11-24 21:11:14 +0100

    Gjorde om view-libraryt till vad det behöver vara.

--- application/libraries/view.php
+++ application/libraries/view.php
@@ -1,26 +1,6 @@
 <?php
 Class view {
-	protected $data = array();
-	protected $template_folder = '';
-	
-	public function __construct() {
-		$this->template_folder = dirname(FCPATH) . '/system/application/views/';
-	}
-	
-	public function __set($name, $value) {
-		$this->data[$name] = $value;
-	}
-	
-	public function &__get($name) {
-		return $this->data[$name];
-	}
-	
 	public function display() {
-		extract($this->data);
-		require($this->template_folder.$this->data['template'].'.php');
-	}
-	
-	public function __isset($name) {
-		return isset($this->data[$name]);
+		get_instance()->load->view($this->template, $this);
 	}
 }
