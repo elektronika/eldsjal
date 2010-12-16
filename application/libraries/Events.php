f1b1b63e1f9c51d97059478a5373e6d65c6de66c
intedinmamma <johnny@nurrd.se>
2010-12-16 13:26:58 +0100

    Events-libraryt, i stort sett ett litet plugin/hooks-system.

---
+++ application/libraries/Events.php
@@ -0,0 +1,30 @@
+<?php
+Class Events extends Library {
+	protected $events = array();
+	
+	public function trigger($event) {
+		$this->event_loop($event, array_slice(func_get_args(), 1));
+	}
+	
+	public function trigger_chained($event) {
+		$args = array_slice(func_get_args(), 1);
+		
+		$args_ref = array();
+		foreach($args as $arg)
+			$args_ref[] =& $arg;
+			
+		$this->event_loop($event, $args_ref);
+				
+		return count($args_ref) > 1 ? $args_ref : current($args_ref);
+	}
+	
+	protected function event_loop($event, $args = array()) {
+		if(isset($this->events[$event]))
+			foreach($this->events[$event] as $callback)
+				call_user_func_array($callback, $args);
+	}
+	
+	public function register($event, $callback) {
+		$this->events[$event][] = $callback;
+	}
+}
