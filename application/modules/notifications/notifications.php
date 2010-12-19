8e329f9362d4241c44cca7479a956fca465b2374
intedinmamma <johnny@nurrd.se>
2010-12-19 23:43:55 +0100

    Så saker slutar fuckas upp...

--- application/modules/notifications/notifications.php
+++ application/modules/notifications/notifications.php
@@ -1,8 +1,8 @@
 <?php
 class NotificationsModule extends Module {
 	public function __construct() {
-		$this->events->register('guestbook_add', array($this, 'guestbook_add'));
-		$this->events->register('message_add', array($this, 'message_add'));
+		// $this->events->register('guestbook_add', array($this, 'guestbook_add'));
+		// $this->events->register('message_add', array($this, 'message_add'));
 	}
 	
 	public function guestbook_add($user, $post) {
