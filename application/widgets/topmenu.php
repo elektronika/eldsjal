74f60beca1c9ece02b73e49911836bfd9a406122
intedinmamma <johnny@nurrd.se>
2010-12-19 18:20:06 +0100

    Lade till så forumet och kalendern också kan ha alerts.

--- application/widgets/topmenu.php
+++ application/widgets/topmenu.php
@@ -3,6 +3,8 @@ class Topmenu extends Widget {
 	public function run() {
 		$this->show_search = $this->session->isLoggedIn();
 		$this->active = $this->uri->segment(1);
+		$this->calendar_alerts = counter($this->alerts->count('events'));
+		$this->forum_alerts = counter($this->alerts->count('forum'));
 		$this->gallery_alerts = counter($this->alerts->count('image_tag'));
 	}
 }
