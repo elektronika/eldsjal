b7598ec11f7409d874c4eccdfbde84beefa06339
intedinmamma <johnny@nurrd.se>
2010-12-21 23:47:32 +0100

    Alerts kan nu ta emot en array av user-id'n, och radering utav alerts sker direkt.

--- application/libraries/alerts.php
+++ application/libraries/alerts.php
@@ -3,7 +3,6 @@ class Alerts extends Library {
 	protected $alerts = array();
 	protected $alert_count = 0;
 	protected $loaded = FALSE;
-	protected $item_ids = array();
 	
 	public function count($type = FALSE) {
 		return $type ? $this->count_type($type) : $this->count_all();
@@ -25,14 +24,21 @@ class Alerts extends Library {
 	}
 	
 	public function item_ids($type) {
-		return isset($this->item_ids[$type]) ? $this->item_ids[$type] : array();
+		return isset($this->alerts[$type]) ? array_keys($this->alerts[$type]) : array();
 	}
 	
 	public function item_has_alert($type, $item_id) {
-		return isset($this->item_ids[$type]) && isset($this->item_ids[$type][$item_id]);
+		return isset($this->alerts[$type][$item_id]);
 	}
 	
 	public function remove($type, $user_id = NULL, $item_id = NULL) {
+		if( ! is_null($item_id)) {
+			if($this->item_has_alert($type, $item_id))
+				unset($this->alerts[$type][$item_id]);
+			else
+				return; // Onödigt att köra databasfrågor om vi vet att dom ändå inte kommer att göra något.
+		}
+		
 		$this->db->where('type', $type);
 		
 		if(is_null($user_id))
@@ -47,7 +53,11 @@ class Alerts extends Library {
 	}
 	
 	public function add($type, $user_id = NULL, $item_id = NULL) {
-		if(is_null($user_id))
+		if(is_array($user_id)) {
+			foreach($user_id as $id)
+				$this->add($type, $id, $item_id);
+			return;
+		} elseif(is_null($user_id))
 			$user_id = $this->session->userId();
 		return $this->db->insert('alerts', array('type' => $type, 'user_id' => $user_id, 'item_id' => $item_id));
 	}
@@ -56,11 +66,11 @@ class Alerts extends Library {
 		if( ! $this->loaded) {
 			$alerts = $this->db->where('user_id', $this->session->userId())->get('alerts')->result();
 			$this->alert_count = count($alerts);
-			foreach($alerts as $alert) {
-				$this->alerts[$alert->type][] = $alert;
+			foreach($alerts as $alert)
 				if( ! empty($alert->item_id))
-					$this->item_ids[$alert->type][$alert->item_id] = $alert->item_id;
-			}
+					$this->alerts[$alert->type][$alert->item_id] = $alert;
+				else
+					$this->alerts[$alert->type][] = $alert;
 			$this->loaded = TRUE;
 		}
 		
